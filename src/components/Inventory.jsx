d4d7f76e95ebe24d966f380187f5df4a8a8c53dc
Cesaum <97255173+oCesaum@users.noreply.github.com>
2022-11-22 13:20:09 -0300

    correção

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -66,7 +66,7 @@ export default function Inventory() {
     }
   }
 
-  const memorizarProdutos = useMemo(() => somaProdutos(somaProdutos), [somaProdutos, estoque]);
+  const memorizarProdutos = useMemo(() => somaProdutos(somaProdutos), [estoque]);
 
 
   function mudarCategoriaNome(e) {
